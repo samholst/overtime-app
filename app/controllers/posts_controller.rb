b6c509e15a9a70c341903917ab74806adb8a74a0
Sam Holst <sawohol@gmail.com>
2017-08-15 16:05:54 -0600

    updated and added for destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at ASC")
   end
 
   def new
@@ -32,6 +32,11 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully.'
   end
 
   private
