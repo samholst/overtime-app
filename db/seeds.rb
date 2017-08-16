210761833e369f0ab822bbb5b91fa48d30081ab2
Sam Holst <sawohol@gmail.com>
2017-08-16 15:37:52 -0600

    implemented enum for status into posts for approval workflow

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 @normal = User.create!(first_name: "Sam", last_name: "Holst", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
 
+puts "1 user created"
+
 @admin = User.create!(first_name: "Sam", last_name: "Holst", email: "admintest@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", type: "AdminUser")
 
-puts "1 user created"
+puts "1 admin created"
+
 
 10.times do |post|
   Post.create!(date: Date.today, rationale: "Day #{post}, I worked way too long! Pay me!", user_id: @normal.id)
