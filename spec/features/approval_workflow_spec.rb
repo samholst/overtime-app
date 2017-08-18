b65aafa41970d6d4e24ad5e66ce2fe18a2dafc20
Sam Holst <sawohol@gmail.com>
2017-08-17 18:07:14 -0600

    implemented ability to block users after post is marked complete

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -20,13 +20,25 @@ describe 'navigate' do
     end
 
     it 'cannot be edited by a non admin' do
-      logout(@admin_user)
+      logout(:user)
       user = FactoryGirl.create(:user)
       login_as(user, :scope => :user)
 
       visit edit_post_path(@post)
 
       expect(page).to_not have_content('Approved')
+    end
+
+    it 'should not be editable by the post creator if status is approved' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+      
+      expect(current_path).to eq(root_path)
     end
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  let(:user) { FactoryGirl.create(:user) }
+
+  let(:post) do
+    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id)
+  end
+
   before do
-    @user = FactoryGirl.create(:user)
-    login_as(@user, :scope => :user)
+    login_as(user, :scope => :user)
   end
 
   describe 'index' do
@@ -27,9 +32,6 @@ describe 'navigate' do
     end
 
     it 'has a scope so that only post creators can see their posts' do
-      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
-      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
-
       other_user = User.create(first_name: 'Non',last_name: 'Authorized',email: "other_user@test.com", password: 'asdfasdf', password_confirmation: 'asdfasdf')
       post_from_other_user = Post.create(date: Date.today, rationale: "Should not be seen!", user_id: other_user.id)
 
@@ -49,11 +51,15 @@ describe 'navigate' do
 
   describe 'delete' do
     it 'can be deleted' do
-      @post = FactoryGirl.create(:post)
-      @post.update(user_id: @user.id)
+      logout(:user)
+
+      delete_user = FactoryGirl.create(:user)
+      login_as(delete_user, :scope => :user)
+
+      post_to_delete = Post.create(date: Date.today, rationale: 'asdf', user_id: delete_user.id)
       visit posts_path
 
-      click_link("delete_post_#{@post.id}_from_index")
+      click_link("delete_post_#{post_to_delete.id}_from_index")
       expect(page.status_code).to eq(200)
     end
   end
