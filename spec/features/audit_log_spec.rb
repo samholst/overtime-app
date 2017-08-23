d47b1424bf2574562968abe1b3d63764f36f45e1
Sam Holst <sawohol@gmail.com>
2017-08-23 16:27:54 -0600

    implemented table for audit logs

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -19,7 +19,12 @@ describe 'AuditLog Feature' do
     end
 
     it 'cannot be accessed by non admin users' do
-      
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      visit audit_logs_path
+      expect(current_path).to eq(root_path)
     end
   end
 end
