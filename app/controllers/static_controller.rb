a9316af33bef70d1ab4c08b6c6f4281e99adf569
Sam Holst <sawohol@gmail.com>
2017-08-24 23:23:16 -0600

    updated static controller queries

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def homepage
-    @pending_approvals = Post.submitted
-    @recent_audit_items = AuditLog.last(10)
+    @pending_approvals = Post.submitted.limit(6)
+    @recent_audit_items = AuditLog.pending.limit(6)
   end
 end
