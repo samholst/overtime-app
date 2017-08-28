5043e098036c8f783615fd402bd9a3e8fb80d326
Sam Holst <sawohol@gmail.com>
2017-08-28 16:13:00 -0600

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,10 +20,10 @@ AuditLog.create!(user_id: @normal.id, status: 0, start_date: (Date.today - 6.day
 AuditLog.create!(user_id: @normal.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: @normal.id, status: 0, start_date: (Date.today - 20.days))
 
-puts "50 posts have been created"
+puts "3 audit logs have been created"
 
 50.times do |post|
   Post.create!(date: Date.today, rationale: "Day #{post}, I worked way too long! Pay me!", user_id: @normal.id, overtime_request: 2.5)
 end
 
-puts "50 audit logs have been created"
+puts "50 posts have been created"
