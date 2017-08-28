0f32164ef76cbca776dd4b3a6f78f0d892dd935a
Sam Holst <sawohol@gmail.com>
2017-08-28 13:34:42 -0600

    added view for employee homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,10 @@
 class StaticController < ApplicationController
   def homepage
-    @pending_approvals = Post.submitted.limit(6)
-    @recent_audit_items = AuditLog.pending.limit(6)
+    if admin_types.include? current_user.type
+      @pending_approvals = Post.submitted.limit(6)
+      @recent_audit_items = AuditLog.pending.limit(6)
+    else
+
+    end
   end
 end
